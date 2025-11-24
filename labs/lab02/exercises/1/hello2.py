38ab0cfbc76e7ffef7ccaf002ac50c7fe1b0a43f
ashwin <aswinakshayvktl@gmail.com>
2025-11-24 22:13:24 +0000

    Clean master branch

--- labs/lab02/exercises/1/hello2.py
+++
@@ -1,9 +0,0 @@
-# hello2.py
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def error():
-    doerror()
-    
--- labs/lab03/variableNumUrl.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/subtract/<int:first>/<int:second>")
-def subtract(first,second):
-    return str(second-first)
